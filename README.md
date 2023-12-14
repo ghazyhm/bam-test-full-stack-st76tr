d7d981b93a220f4e624783be50211d96fa152fdf
ghazyhm <102327494+ghazyhm@users.noreply.github.com>
2023-12-14 19:45:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bam-test-full-stack-st76tr
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/ghazyhm/bam-test-full-stack-st76tr)
---
+++ index.js
@@ -0,0 +1,73 @@
+// Import stylesheets
+import './helpers/style.css';
+import * as helper from './helpers/helpers.js';
+
+// VOTRE NOM ET PRENOM ICI ! Hamza EL GHAZY
+
+function solutionExercice1(N) {
+  var res = 'EXERCICE 1';
+  //TAPE LE CODE DE L'EXERCICE 1 ICI!
+  for (let i = N; i >= 1; i--) {
+    const digits = String(i).split('').map(Number);
+
+    let isRanged = true;
+    for (let j = 0; j < digits.length - 1; j++) {
+      if (digits[j] > digits[j + 1]) {
+        isRanged = false;
+        break;
+      }
+    }
+
+    if (isRanged) {
+      res = i;
+      break;
+    }
+  }
+
+  return res;
+}
+
+function solutionExercice2(L) {
+  var res = 'EXERCICE 2';
+  //TAPE LE CODE DE L'EXERCICE 2 ICI!
+  L.sort((a, b) => a - b);
+
+  let smallestPositive = 1;
+
+  // Parcourez la liste triée pour trouver le plus petit entier positif manquant
+  for (const num of L) {
+    if (num === smallestPositive) {
+      smallestPositive++;
+    } else if (num > smallestPositive) {
+      break;
+    }
+  }
+  return smallestPositive;
+}
+function solutionExercice3(L) {
+  var res = 'EXERCICE 3';
+  //TAPE LE CODE DE L'EXERCICE 3 ICI!
+  var occurrences = {};
+
+  // Parcours de la liste pour compter les occurrences de chaque élément
+  for (var i = 0; i < L.length; i++) {
+    var currentElement = L[i];
+    // Si l'élément existe déjà dans la hashmap, on incrémente son compteur
+    // Sinon, on l'ajoute à la hashmap avec un compteur initialisé à 1
+    occurrences[currentElement] = (occurrences[currentElement] || 0) + 1;
+  }
+
+  // Parcours de la hashmap pour trouver l'élément unique
+  for (var key in occurrences) {
+    if (occurrences[key] === 1) {
+      res = key;
+      break;
+    }
+  }
+
+  return res;
+}
+
+helper.displayOnHtml(solutionExercice1(1000), '1');
+helper.displayOnHtml(solutionExercice2([1, 4, 7, 5, 3, 6, 3]), '2');
+helper.displayOnHtml(solutionExercice3([1, 5, 3, 6, 4, 3, 1, 5, 4]), '3');
